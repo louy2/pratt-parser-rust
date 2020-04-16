5fa21393726baf4ebb8e1a9f3646e373f6d84aab
Yufan Lou <2263580+louy2@users.noreply.github.com>
2020-04-16 09:57:07 -0400

    Add lexer

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,39 @@
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Token {
+	Atom(char),
+	Op(char),
+	Eof,
+}
+
+struct Lexer {
+	tokens: Vec<Token>,
+}
+
+impl Lexer {
+	fn new(input: &str) -> Lexer {
+		let tokens: Vec<_> = input
+			.chars()
+			.filter(|it| !it.is_ascii_whitespace())
+			.map(|c| match c {
+				'0'..='9' | 'a'..='z' | 'A'..='Z' => Token::Atom(c),
+				_ => Token::Op(c),
+			})
+			.rev()
+			.collect();
+		Lexer { tokens }
+	}
+}
+
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+	use crate::*;
+	#[test]
+	fn lexer_new() {
+		use crate::Token::*;
+		let case = "1 + 2";
+		let expected = Lexer {
+			tokens: vec![Atom('2'), Op('+'), Atom('1')],
+		};
+		assert_eq!(Lexer::new(case).tokens, expected.tokens);
+	}
 }
